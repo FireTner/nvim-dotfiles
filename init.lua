5cdf3e02d222899582cca1e3e036804408e12e27
FireTner <49531229+FireTner@users.noreply.github.com>
2023-05-08 22:14:13 +0200

    Initial commit
    I forgot init.lua

---
+++ init.lua
@@ -0,0 +1,29 @@
+vim.opt.tabstop = 3
+vim.opt.shiftwidth = 3
+vim.opt.expandtab = true
+
+vim.wo.relativenumber = true
+
+vim.opt.cmdheight = 0
+vim.opt.laststatus = 3
+
+vim.opt.clipboard = unnamedplus
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+
+if not vim.loop.fs_stat(lazypath) then
+   vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      "https://github.com/folke/lazy.nvim.git",
+      "--branch=stable", -- latest stable release
+      lazypath,
+   })
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(require("plugins"), {
+   install = { colorscheme = { "tokyonight" } }
+})
